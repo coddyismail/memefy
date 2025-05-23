1eb3b48badd012b4b136ef6e4be6e939ab6f9bd8
Ismail Shaikh <91933697+coddyismail@users.noreply.github.com>
2025-05-23 16:19:34 +0530

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,17 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: '/ffmpeg/ffmpeg-core.js',
+        headers: [
+          { key: 'Content-Type', value: 'text/javascript' },
+          { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
+        ],
+      },
+      {
         source: '/ffmpeg/ffmpeg-core.wasm',
         headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/wasm',
-          },
+          { key: 'Content-Type', value: 'application/wasm' },
+          { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
         ],
       },
     ];
